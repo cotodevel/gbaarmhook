5574098b69373f32dd7d208d3fdac675a95f0e5f
Coto <programmer.coto.1988@gmail.com>
2020-05-14 02:16:26 -0200

    Update Binary.
    TGDS Changes:
    
    - Allow TGDS projects to initialize ARM7/ARM9 properly by syncing init code to VRAM bits. Fixes booting up TGDS projects almost everywhere.
    Note: some emulators don't emulate some ARM7 bits (TGDS irqs/wifi) which do work in real hardware, thus these cause black screens and will prevent from booting
    TGDS Projects.

--- arm7/include/main.h
+++ arm7/include/main.h
@@ -20,7 +20,7 @@ USA
 #include "typedefsTGDS.h"
 #include "dsregs.h"
 #include "dsregs_asm.h"
-
+#include "soundTGDS.h"
 #endif
 
 
@@ -34,7 +34,11 @@ extern u8* soundbuffB;
 
 extern u8 DMA_A_TIMERSEL;
 extern u8 DMA_B_TIMERSEL;
-
+extern u16 strpcmL0Buf[WAV_READ_SIZE];
+extern u16 strpcmL1Buf[WAV_READ_SIZE];
+extern u16 strpcmR0Buf[WAV_READ_SIZE];
+extern u16 strpcmR1Buf[WAV_READ_SIZE];
+extern void initSoundStreamUser();
 #ifdef __cplusplus
 }
 #endif
